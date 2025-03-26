8cdeee05564aeec3cc1ffc6d4ff298ac6f9fac65
Nemanja Klinic <klinicnemanja@gmail.com>
2025-03-26 17:07:29 +0100

    feat: added prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
-  return (
-    <View className="bg-dark flex-1 justify-center items-center">
-      <Text className="text-6xl text-accent font-bold">Welcome!</Text>
-    </View>
-  );
+	return (
+		<View className='bg-dark flex-1 justify-center items-center'>
+			<Text className='text-6xl text-accent font-bold'>Welcome!</Text>
+		</View>
+	);
 }
